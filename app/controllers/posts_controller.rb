d1cdd397a84213665aebaf488ebc80ee826a4ad0
DiLivius <di_livius@yahoo.co.uk>
2016-11-01 09:27:35 +0200

    Added edit and update for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("id ASC")
   end
 
   def new
@@ -20,13 +20,17 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Your post was successfully created!'
+    else
+      render :edit
+    end
   end
 
   private
