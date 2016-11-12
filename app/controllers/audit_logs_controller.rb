fcb8df8d27ae6da998056cd8a2dca1823b334cc2
DiLivius <di_livius@yahoo.co.uk>
2016-11-12 14:53:02 +0200

    Allowed only admins to access audit_logs index page

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -1,6 +1,9 @@
 class AuditLogsController < ApplicationController
   def index
+    # @audit_logs = AuditLog.audit_log_by current_user
+    # @audit_logs = current_user.audit_logs.order("id ASC")
     @audit_logs = AuditLog.all
+    authorize @audit_logs
   end
 
 end
