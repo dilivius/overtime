d26207fac43809b0880781dc157b449e3e13dfdc
DiLivius <di_livius@yahoo.co.uk>
2016-11-13 12:51:48 +0200

    Added approving capabilities for admins

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,16 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :approve]
 
   def index
     # @posts = Post.all.order("id ASC")
     # @posts = current_user.posts.order("id ASC")
     @posts = Post.posts_by(current_user).page(params[:page]).per(10)
+  end
+
+  def approve
+    authorize @post
+    @post.approved!
+    redirect_to root_path, notice: 'The post has been approved!'
   end
 
   def new
